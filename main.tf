b14353fcaf122aececba121874137a6813a6d84d
boriowo <100302941+Boriowo@users.noreply.github.com>
2022-07-22 09:52:31 +0100

    Data Platform

--- main.tf
+++ main.tf
@@ -8,9 +8,11 @@ module "ETL-Job" {
   name         = "catalogdatabase"
   catalogtable = "mycatalogtable"
   crawlername  = "example"
+  #role = "aws_iam_role.example.arn"
   schedule     = ""
   table_prefix = ""
   description  = ""
+  #s3_path = "s3://${aws_s3_bucket.exam.bucket}"
   classifier    = "example"
   gluename      = "example"
   gluejobscript = "s3://aws-glue-assets-431617346510-us-east-1/scripts/Untitled job.py"
@@ -25,7 +27,7 @@ module "ETL-Job" {
 
 #calling the data warehouse module
 module "Warehouse" {
-  source          = "./Data-Warehousing"
+  source          = "./Data-Warehouse"
   region          = "us-east-1"
   clusterid       = "oluwa-redshift-cluster"
   dbname          = "mydb"
@@ -34,8 +36,7 @@ module "Warehouse" {
   rolename        = "moses_role"
 }
 
-#calling the data query module to query the resources created
-# and visualize them on Quicksight
+#calling the data query module
 module "Query" {
   source          = "./Data-Modelling"
   region          = "us-east-1"
